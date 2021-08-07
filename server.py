f8c67bc4abba77c546d3de92b798165511c4843f
Jeremy Bell <jeremy.bell@coderacademy.edu.au>
2021-08-07 13:50:06 +1000

    First commit, added server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+import socket                       
+
+try:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # print(type(server_socket))
+
+    port_number = 65432
+
+    server_socket.bind(("127.0.0.1", port_number))
+
+    server_socket.listen()
+
+    print(f"Listening for incoming connection on port {port_number}")
+
+    connection, address = server_socket.accept()
+
+    print(f"Connected by {address}.")
+
+    while True:
+
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data.upper())
+
+    server_socket.close()
+except KeyboardInterrupt:
+    print("Exiting \n")
+    exit(0)
+
+    
